7511ea72cc7dfa5b9484fb40ef76db5fbc24dcf8
dgriego <daniel@jackrabbitsystems.com>
2014-08-28 22:19:37 -0600

    update controller name

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -8,7 +8,7 @@
  * Controller of the battleshipApp
  */
 angular.module('battleshipApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('BoardCtrl', function ($scope) {
     $scope.board = {
         o : '<td>o</td>',
         hit : '<td>x</td>'
